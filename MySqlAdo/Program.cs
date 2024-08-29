303350230a57100f12b9facacebf773d944c1a3f
zerrinayaz76@gmail.com <zerrinayaz76@gmail.com>
2024-08-29 23:26:34 +0300

    Created ADO connection for Mysql

--- MySqlAdo/Program.cs
+++ MySqlAdo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySqlConnector;
+using System;
 
 namespace MySqlAdo
 {
@@ -6,7 +7,26 @@ namespace MySqlAdo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string constr = "Server=localhost;Database=sakila;Uid=root;Pwd=qweasd;";
+            MySqlConnection conn = new(constr);
+            MySqlCommand cmd = conn.CreateCommand();
+
+            #region Table reading
+            cmd.CommandText = "Select * from city";
+            conn.Open();
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+                Console.WriteLine(reader["city"]);
+            #endregion
+
+            #region Single Value Reading
+            conn.Open();
+            cmd.CommandText = "select count(*) Adet from City";
+            var res = cmd.ExecuteScalar();
+            Console.WriteLine("Kayit Sayisi:" + res);
+            #endregion
+            conn.Close();
         }
     }
 }
